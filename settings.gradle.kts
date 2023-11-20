f3d389442a09959a7e51a2d0580239e566131463
vinh <vinhintw2014@gmail.com>
2023-11-20 21:01:13 +0800

    create app

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "APPFinalProject _11131415"
+include(":app")
