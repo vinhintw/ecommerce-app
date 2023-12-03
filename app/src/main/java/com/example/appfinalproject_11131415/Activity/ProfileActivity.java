94816e6efb22313b76f90eb9c97b2c48a885a7b5
vinh <vinhintw2014@gmail.com>
2023-12-03 11:18:31 +0800

    add ProfileActivity

---
+++ app/src/main/java/com/example/appfinalproject_11131415/Activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.appfinalproject_11131415.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appfinalproject_11131415.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile);
+	}
+}
